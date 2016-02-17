761e322ead44acc18f36b27e9d44fe17b6ac70c5
Nirmal Almara <rkumarnirmal@gmail.com>
2016-02-17 15:57:06 +0530

    setting up server

--- engine.py
+++ engine.py
@@ -1,17 +1,12 @@
-import os
-import redis
-import urlparse
-from werkzeug.wrappers import Request, Response
-from werkzeug.routing import Map, Rule
-from werkzeug.exceptions import HTTPException, NotFound
-from werkzeug.wsgi import SharedDataMiddleware
-from werkzeug.utils import redirect
-from jinja2 import Environment, FileSystemLoader
+from flask import Flask
+app = Flask(__name__)
 
-if __name__ == '__main__':
-    from werkzeug.serving import run_simple
-    app = create_app()
-    run_simple('127.0.0.1', 1935, app, use_debugger=True, use_reloader=True)
+@app.route("/")
+def hello():
+  return "Welcome to Python Flask!"
+
+if __name__ == "__main__":
+  app.run()
 
 
 
